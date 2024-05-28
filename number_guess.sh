d7afe868a6dfb3495651cdb99231d4117a02dd54
Nebal Maysaud <nmaysaud@gmail.com>
2024-05-28 19:18:28 +0000

    fix: create database, fix script

--- number_guess.sh
+++ number_guess.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 echo "Enter your username:"
 read USERNAME
@@ -19,10 +20,61 @@ START_GAME() {
   fi
 
   read GUESS
+  echo $GUESS
 }
 
+# if username is not in the database
+if [[ -z $($PSQL "SELECT username FROM users WHERE username='$USERNAME'") ]]
+then
+  # add user to database
+  $PSQL "INSERT INTO users(username) VALUES('$USERNAME')"
+
+  # welcome user
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+
+else
+
+  # get user id
+
+  # get games played
+
+  # get best game
+
+  # welcome user
+  echo -e "\nWelcome back $USERNAME!"
+fi
+
+# start game
+START_GAME
+
+# If guess is not a number
+if [[ ! $GUESS =~ ^[0-9]+$ ]]
+then
+  # restart game
+  START_GAME "That is not an integer, guess again:"
+
+# initiate for loop
+else
+  for (( i = 0; $GUESS = $WINNING_NUMBER; i++ )) {
+    
+    # if number of too low
+    if (( $GUESS < $WINNING_NUMBER ))
+    then
+      echo -e "It's lower than that, guess again:"
+    else
+      echo -e "It's higher than that, guess again:"
+    fi
+
+    # change guess
+    $GUESS
+
+    # test if guess actually changed
+    echo -e "your new guess is $GUESS"
+  }
+fi
 
 
+: '
   # initiate for loop
   for (( i=0; $GUESS=$WINNING_NUMBER; i++ )) {
 
@@ -61,3 +113,4 @@ else
 
   fi
 fi
+'
