f97f1181e2eb8385e146d65766c5ffb02b6e59ec
Nebal Maysaud <nmaysaud@gmail.com>
2024-05-28 17:52:00 +0000

    feat: set up start game, username, and guess initialization

--- number_guess.sh
+++ number_guess.sh
@@ -1,2 +1,63 @@
 #!/bin/bash
 
+echo "Enter your username:"
+read USERNAME
+
+START_GAME() {
+  # Generate a random number as $WINNING_NUMBER, check variable scope
+
+  # if no argument
+  if [[ -z $1 ]]
+  then
+    # Tell user to guess a number
+    echo -e "\nGuess the secret number between 1 an 1000:"
+
+  else
+    # Enter arg as prompt
+    echo -e "\n$1"
+
+  fi
+
+  read GUESS
+}
+
+
+
+  # initiate for loop
+  for (( i=0; $GUESS=$WINNING_NUMBER; i++ )) {
+
+  }
+
+# if username is not in the database
+if [[ $USERNAME != $($PSQL "SELECT username FROM users WHERE username='$USERNAME'")]]
+then
+  # add user to database
+  $PSQL "INSERT INTO users(username) VALUES('$USERNAME')"
+
+  # welcome user
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+else
+  # get user id
+
+  # get games played
+
+  # get best game
+
+  # welcome user
+  echo -e "\nWelcome back $USERNAME!"
+
+  # start game
+  START_GAME
+
+  # If guess is not a number
+  if [[ ! $GUESS =~ ^[0-9]+$ ]]
+  then
+    # restart game
+    START_GAME "That is not an integer, guess again:"
+
+  #if guess is a match
+
+  #if guess is too low
+
+  fi
+fi
