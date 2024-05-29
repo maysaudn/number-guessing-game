d689e3a486524a72905ffbe51075d833e803776a
Nebal Maysaud <nmaysaud@gmail.com>
2024-05-29 19:33:46 +0000

    fix: cleaned up db mess

--- number_guess.sh
+++ number_guess.sh
@@ -26,11 +26,20 @@ else
   GAMES_PLAYED=$($PSQL "
     SELECT COUNT(game_id) FROM games WHERE user_id=$USER_ID
   ")
+
   # get best game
+  BEST_GAME=$($PSQL "
+    SELECT MIN(no_of_guesses) FROM games WHERE user_id=$USER_ID
+  ")
 
   # welcome user
-  echo -e "\nWelcome back $USERNAME!"
+  echo -e "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
+
+# get user id
+USER_ID=$($PSQL "
+  SELECT user_id FROM users WHERE username='$USERNAME'
+")
 
 # Generate a random number as $WINNING_NUMBER, check variable scope
 WINNING_NUMBER=$[ $RANDOM % 1000 + 0 ]
@@ -65,153 +74,9 @@ else
     read GUESS
 
   done
-
-  echo -e "\nYou guessed it in $i tries. The secret number was $WINNING_NUMBER. Nice job!"
-
   # add game to database
-  $PSQL "INSERT INTO games(user_id, no_of_guesses) VALUES($USER_ID, $i)"
+  $PSQL "INSERT INTO games(user_id, no_of_guesses) VALUES($USER_ID, $i)" | sed '"s/INSERT 0 1//'
+
+  echo -e "You guessed it in $i tries. The secret number was $WINNING_NUMBER. Nice job!"
 
 fi
-
-
-: '
-
-START_GAME() {
-  # Generate a random number as $WINNING_NUMBER, check variable scope
-  WINNING_NUMBER=$[ $RANDOM % 1000 + 0 ] 
-
-  # test if that worked
-  echo -e "Winning Number is $WINNING_NUMBER"
-
-  # if no argument
-  if [[ -z $1 ]]
-  then
-    # Tell user to guess a number
-    echo -e "\nGuess the secret number between 1 and 1000:"
-
-  else
-    # Enter arg as prompt
-    echo -e "\n$1"
-
-  fi
-
-  read GUESS
-  # If guess is not a number
-  if [[ ! $GUESS =~ ^[0-9]+$ ]]
-  then
-    # restart game
-    START_GAME "That is not an integer, guess again:"
-
-    # initiate for loop
-  else
-    for (( i = 0; $GUESS == $WINNING_NUMBER; i++ )) {
-    
-    # if number of too low
-    if (( $GUESS < $WINNING_NUMBER ))
-    then
-      echo -e "It's lower than that, guess again:"
-    else
-      echo -e "It's higher than that, guess again:"
-    fi
-
-    # change guess
-    $GUESS
-
-    # test if guess actually changed
-    echo -e "your new guess is $GUESS"
-  }
-fi
-
-}
-
-# if username is not in the database
-if [[ -z $($PSQL "SELECT username FROM users WHERE username='$USERNAME'") ]]
-then
-  # add user to database
-  $PSQL "INSERT INTO users(username) VALUES('$USERNAME')"
-
-  # welcome user
-  echo "Welcome, $USERNAME! It looks like this is your first time here."
-
-else
-
-  # get user id
-
-  # get games played
-
-  # get best game
-
-  # welcome user
-  echo -e "\nWelcome back $USERNAME!"
-fi
-
-# start game
-START_GAME
-
-# If guess is not a number
-if [[ ! $GUESS =~ ^[0-9]+$ ]]
-then
-  # restart game
-  START_GAME "That is not an integer, guess again:"
-
-# initiate for loop
-else
-  for (( i = 0; $GUESS == $WINNING_NUMBER; i++ )) {
-    
-    # if number of too low
-    if (( $GUESS < $WINNING_NUMBER ))
-    then
-      echo -e "It's lower than that, guess again:"
-    else
-      echo -e "It's higher than that, guess again:"
-    fi
-
-    # change guess
-    $GUESS
-
-    # test if guess actually changed
-    echo -e "your new guess is $GUESS"
-  }
-fi
-
-'
-: '
-  # initiate for loop
-  for (( i=0; $GUESS=$WINNING_NUMBER; i++ )) {
-
-  }
-
-# if username is not in the database
-if [[ $USERNAME != $($PSQL "SELECT username FROM users WHERE username='$USERNAME'")]]
-then
-  # add user to database
-  $PSQL "INSERT INTO users(username) VALUES('$USERNAME')"
-
-  # welcome user
-  echo "Welcome, $USERNAME! It looks like this is your first time here."
-else
-  # get user id
-
-  # get games played
-
-  # get best game
-
-  # welcome user
-  echo -e "\nWelcome back $USERNAME!"
-
-  # start game
-  START_GAME
-
-  # If guess is not a number
-  if [[ ! $GUESS =~ ^[0-9]+$ ]]
-  then
-    # restart game
-    START_GAME "That is not an integer, guess again:"
-
-  #if guess is a match
-
-  #if guess is too low
-
-  fi
-fi
-'
